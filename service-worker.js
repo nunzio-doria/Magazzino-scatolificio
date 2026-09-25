6984697fafe291eff65cd44362a3149f1af98f55
nunzio-doria <nunziodoria96@gmail.com>
2026-09-25 12:55:41 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@
 // non succede nulla di grave: semplicemente niente copia offline.
 // =============================================================
 
-const CACHE_NAME = 'magazzino-shell-v28';
+const CACHE_NAME = 'magazzino-shell-v29';
 const APP_SHELL = [
   './',
   './index.html',
